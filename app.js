1eb82dcf2fa37151e044ca8d62032d18b57aaafb
Calin Tif <culitif@tuta.io>
2021-02-12 15:19:11 -0800

    added js functionality to the button press

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 // import functions and grab DOM elements
 
-// initialize state
-
-// set event listeners to update state and DOM
-
-import { sync } from './dom-utilities';
+import { sync } from './dom-utilities.js';
 
 const submitButton = document.getElementById('button-main');
 
-submitButton.addEventListener('click', () => {});
+// initialize state
 
 const syncInit = () => {
     for (let i = 1; i < 14; i++) {
         const inputElement = document.getElementById(`input-${i}`);
         const spanElement = document.getElementById(`blank-${i}`);
         inputElement.placeholder = spanElement.textContent;
-    }
+    };
 };
 
 syncInit();
 
-document.addEventListener('DOMContentLoaded', syncInit());
+// set event listeners to update state and DOM
+
+submitButton.addEventListener('click', () => {
+    for (let i = 1; i < 14; i++) {
+        // check that the inputs are actually filled out
+        const inputElement = document.getElementById(`input-${i}`);
+        const spanElement = document.getElementById(`blank-${i}`);
+        sync(inputElement, spanElement);
+    };
+
+    // add the hidden class to the madlib story section 
+});
